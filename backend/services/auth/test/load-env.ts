d187f618237b7175b89ccdde95be78fb267d9020
ThiagoCanatoAzevedo <thi.canato@gmail.com>
2026-05-18 22:08:19 -0300

    fix: changes test setup

--- backend/services/auth/test/load-env.ts
+++ backend/services/auth/test/load-env.ts
@@ -1,9 +1,12 @@
 import * as dotenv from 'dotenv'
 import * as path from 'path'
 
+const envPath = path.resolve(__dirname, '../.env.test')
+console.log('__dirname:', __dirname)
+console.log('envPath:', envPath)
+
 dotenv.config({ 
-  path: path.resolve(__dirname, '../.env.test'),
+  path: envPath,
   override: true
 })
-
 console.log('JWT:', process.env.JWT_ACCESS_SECRET)
